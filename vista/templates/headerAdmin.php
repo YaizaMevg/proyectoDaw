3af01c8e8f6424acbdd820faa18b5d4a8e7ae465
YaizaMevg <yaiza.mevg@gmail.com>
2016-05-27 08:33:57 +0200

    Add files via upload

---
+++ vista/templates/headerAdmin.php
@@ -0,0 +1,68 @@
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+	<meta charset="utf-8"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	<title>ClosetCloud</title>
+	<link rel="stylesheet" href="../css/foundation.min.css">
+        <link rel="stylesheet" href="../css/foundation-icons/foundation-icons.css"/>
+        <link rel="stylesheet" href="../css/css.css">
+        <link rel="stylesheet" href="../css/cssIconosSociales.css">
+       
+        
+        <link rel="stylesheet" href="../css/footer.css">
+        <link rel="icon" type="image/png" href="../imagenes/favicon.ico" />
+</head>
+	<body >
+	 <!-- Menú superior -->
+                <div class="top-bar">
+			<div class="top-bar-left">
+				<ul class="menu">
+                                    <a href="inici.php"><img src="../imagenes/logo.png" width="100px" height="50px"/></a>
+					
+				</ul>
+			</div>
+			<div class="top-bar-right">
+				<ul class="menu">
+                                    <ul class="menu">
+                                        <!--<li><input type="search" placeholder="Search"></li>-->
+                                        
+                                        <li><span class="prefix"><i class="fi-shopping-cart" id="carrito"></i></span></li>
+					<li><a href="registroUser.php">Entra/Registrate</a></li>
+                                        
+				</ul>
+			</div>
+		</div>
+		<div class="top-bar">
+			<div class="top-bar-left">
+				<ul class="dropdown menu" data-dropdown-menu>
+					
+					<li class="has-submenu">
+                                            <a href="anadirProducto.php">Añadir Producto</a>
+                                              
+                                        
+                                        </li>
+                                        <li class="has-submenu">
+                                            <a href="eliminarProducto.php">Modificar Producto</a>
+                                        </li>
+                                        <li class="has-submenu">
+                                            <a href="modificarProducto.php">Eliminar Producto</a>
+                                        </li>
+                                                                              
+					<li class="has-submenu">
+                                           <a href="reclamaciones.php">Reclamaciones</a>
+                                        </li>
+                                       
+                                       
+				</ul>
+			</div>
+			<div class="top-bar-right">
+				<ul class="menu">
+                                    <ul class="menu">
+                                        <li><input type="search" placeholder="Search"></li>
+					
+				</ul>
+			</div>
+		</div>
+         
+        
