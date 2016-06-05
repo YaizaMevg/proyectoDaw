504d65dfb82b6788defe4ffa5b04358a3f5d4ccc
YaizaMevg <yaiza.mevg@gmail.com>
2016-06-05 17:29:38 +0200

    Add files via upload

---
+++ archivosnuevos/header.php
@@ -0,0 +1,81 @@
+<!doctype html>
+<html class="no-js" lang="en">
+    <head>
+        <meta charset="UTF-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <title>ClosetCloud</title>
+        <link rel="stylesheet" href="css/foundation.min.css">
+        <link rel="stylesheet" href="css/foundation-icons/foundation-icons.css"/>
+        <link rel="stylesheet" href="css/css.css">
+        <link rel="stylesheet" href="css/cssIconosSociales.css">
+        <link rel="stylesheet" href="css/footer.css">
+        <link rel="icon" type="image/png" href="imagenes/favicon.ico" />
+    </head>
+    <body >
+        <!-- Menú superior -->
+        <div class="top-bar">
+            <div class="top-bar-left">
+                <ul class="menu">
+                    <a href="inici.php"><img src="imagenes/logo.png" width="100px" height="50px"/></a>
+
+                </ul>
+            </div>
+            <div class="top-bar-right">
+                <ul class="menu">
+                    <ul class="menu">
+                        <!--<li><input type="search" placeholder="Search"></li>-->
+
+                        <li>
+                            <span class="prefix">
+                                <a href="carritodecompras.php" title="ver carrito de compras">
+                                    <i class="fi-shopping-cart" id="carrito"></i>
+                                </a>
+                            </span>
+                        </li>
+                        <li><a href="registroUser.php">Entra/Registrate</a></li>
+
+                    </ul>
+            </div>
+        </div>
+        <div class="top-bar">
+            <div class="top-bar-left">
+                <ul class="dropdown menu" data-dropdown-menu>
+                    <li class="has-submenu">
+                        <a href="#">HOMBRE</a>
+                        <ul class="submenu menu vertical" data-submenu>
+                            <li><a href="camiHombre.php">Camisa</a></li>
+                            <li><a href="#">Pantalon</a></li>
+                            <li><a href="#">Sudaderas</a></li>
+                        </ul>
+
+                    </li>
+                    <li class="has-submenu">
+                        <a href="#">MUJER</a>
+                        <ul class="submenu menu vertical" data-submenu>
+                            <li><a href="camiMujer.php">Camisa</a></li>
+                            <li><a href="#">Pantalon</a></li>
+                            <li><a href="#">Sudaderas</a></li>
+                        </ul>
+
+                    </li>
+                    <li class="has-submenu">
+                        <a href="#">KIDS</a>
+                        <ul class="submenu menu vertical" data-submenu>
+                            <li><a href="kids.php">Camisa</a></li>
+                            <li><a href="#">Pantalon</a></li>
+                            <li><a href="#">Sudaderas</a></li>
+                        </ul>
+
+                    </li>
+                    <li><a href="nosotros.php">NOSOTROS</a></li>
+                    <li><a href="contacto.php">CONTACTO</a></li>
+                </ul>
+            </div>
+            <div class="top-bar-right">
+                <ul class="menu">
+                    <ul class="menu">
+                        <li><input type="search" placeholder="Search"></li>
+
+                    </ul>
+            </div>
+        </div>
